46efb2046ef139542ba77713d66728e66fe0818d
HarmonyMurombo <harmonysvinurai1@gmail.com>
2025-10-14 17:27:57 +0200

    latest commints wifi

--- components/customer/PackageDetails.tsx
+++ components/customer/PackageDetails.tsx
@@ -277,7 +277,7 @@ export default function PackageDetails({
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              Recipient's phone will be automatically added to package description and sent to driver separately
+              Recipient's phone will be automatically
             </p>
           </div>
 
